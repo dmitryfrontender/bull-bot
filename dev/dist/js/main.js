5b937e49e12cd9fdcaf562ac25a3838627418f14
dmitryfrontender <dmitry.nasyrkulov@gmail.com>
2024-12-17 06:46:30 +0200

    done without adaptiv

--- dev/dist/js/main.js
+++ dev/dist/js/main.js
@@ -1,29 +1,34 @@
 $(document).ready(function(){
 	//calls functions
 	plugOpenPopup();
+	claimOpenPopup();
 	plugClosePopup();
-	timerFunction();
+	infoOpenPopup();
+	timerPlugFunction();
+	initSwiper();
+	tabsInit();
+	startMining();
 });
-
-// $(window).on('load', function () {
-// 	//calls functions
-// });
-
-// $(window).on('resize', function () {
-// 	//calls functions
-// });
-
-// $(window).load(function() {
-// 	//calls functions
-// });
-
-// $(window).scroll(function() {
-// 	//calls functions
-// });
 
 //functions
 const plugOpenPopup = () => {
 	const joinBtn = $('.js-join-btn');
+	const popupBlock = $('.popup-block');
+	joinBtn.on('click', () => {
+		popupBlock.addClass('active');
+	});
+}
+
+const claimOpenPopup = () => {
+	const joinBtn = $('.js-claim-button');
+	const popupBlock = $('.popup-block');
+	joinBtn.on('click', () => {
+		popupBlock.addClass('active');
+	});
+}
+
+const infoOpenPopup = () => {
+	const joinBtn = $('.js-info-button');
 	const popupBlock = $('.popup-block');
 	joinBtn.on('click', () => {
 		popupBlock.addClass('active');
@@ -41,9 +46,55 @@ const plugClosePopup = () => {
 	});
 }
 
-const timerFunction = () => {
+const timerPlugFunction = () => {
 	$('#simple_timer').syotimer({
 		date: new Date(2035, 4, 9, 20, 30),
 	});
+};
 
+const timerFunction = () => {
+	$('#simple_mine_timer').syotimer({
+		date: new Date(2024, 11, 26, 20, 30, 0),
+		layout: "hms",
+	});
+};
+
+const startMining = () => {
+	const startBtn = $('.js-start-mining');
+	const wrapperBlock = $('.app-wrapper');
+	startBtn.on('click', () => {
+		if (wrapperBlock.hasClass('mining-started')) return;
+		wrapperBlock.addClass('mining-started');
+		timerFunction();
+	});
+}
+
+const initSwiper = () => {
+	const swiper = new Swiper('.swiper', {
+		// Optional parameters
+		loop: false,
+		slidesPerView: 2,
+		slidesPerGroup: 2,
+      	spaceBetween: 10,
+
+		// If we need pagination
+		pagination: {
+		  el: '.swiper-pagination',
+		},
+
+		// Navigation arrows
+		navigation: false,
+	});
+};
+
+const tabsInit = () => {
+	const tabs = document.querySelectorAll('.tab');
+	const contents = document.querySelectorAll('.tabcontent');
+
+	tabs.forEach(tab => {
+		tab.addEventListener('click', () => {
+			contents.forEach(content => content.classList.remove('active'));
+			document.getElementById(tab.dataset.content)?.classList.add('active');
+		});
+	});
 };
